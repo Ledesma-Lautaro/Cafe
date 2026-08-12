cfc90717d75ddcf662c6c69e3b73099ad92669ce
Lautaro Ledesma <ledesmalautaroezequiel02@gmail.com>
2026-08-12 13:34:31 -0300

    Cambiado home por profile en redirect de login para pruebas

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -27,7 +27,7 @@ export default function LoginPage() {
         return;
       }
 
-      router.push("/home");
+      router.push("/profile");
     } finally {
       setIsSubmitting(false);
     }
